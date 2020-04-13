8320f11bc4648fb8dfdba4cabd78976f76abe173
enigma49 <shaikhhamza157@gmail.com>
2020-04-14 04:41:50 +0530

    Updates

---
+++ precache-manifest.2ee7199c12fdbeb6380d5bc9dc8180dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff8191d736eddb0fb62feceff31ea50",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "ed8a2b32079d01e7f9c5",
+    "url": "/covid/static/css/main.ba3990a5.chunk.css"
+  },
+  {
+    "revision": "c42dfd33761fdb5763af",
+    "url": "/covid/static/js/2.4cd3d273.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.4cd3d273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8a2b32079d01e7f9c5",
+    "url": "/covid/static/js/main.8e0aba10.chunk.js"
+  },
+  {
+    "revision": "29d887c2aea3fb65dc6d",
+    "url": "/covid/static/js/runtime-main.b3dccd4d.js"
+  }
+]);
